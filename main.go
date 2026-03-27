c183149e49fac19f9bc5b06ee88fbf75b62e3b6d
Airaz <airaz.khan@roadcast.in>
2026-03-27 11:33:19 +0530

    Refactor main.go to enhance ticket booking functionality and remove data-types-tut3.go

--- main.go
+++ main.go
@@ -1,23 +1,70 @@
 package main
-import "fmt"
 
-func main(){
+import (
+	"fmt"
+	"strings"
+)
 
-	 conferenceName := "Go Conference"
-	const conferenceTickets int= 50 
-	var remainingTickets uint= 50
+func main() {
+
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
-	fmt.Printf("Welcome to %v booking application\n",conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n",conferenceTickets,remainingTickets)
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string;
-	var userTickets int;
-	// ask user for their name
+	for {
+		var Firstname string
+		var LastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&Firstname)
 
-	userName = "Mark"
-	userTickets = 2
-	fmt.Printf("User name is %v and user tickets is %v\n",userName,userTickets)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&LastName)
+
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter your tickets:")
+		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickets{
+			fmt.Printf("We only have %v tickets remaining , so you can't book %v tickets\n", remainingTickets, userTickets)
+			continue
+
+		}
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, Firstname+" "+LastName)
+
+		fmt.Printf("The slice array: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+		fmt.Printf("Type of Slice: %T\n", bookings[0])
+		fmt.Printf("Size of Slice: %v\n", len((bookings)))
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",
+			Firstname, LastName, userTickets, email)
+		fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+
+		for _, booking := range bookings{
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames,names[0])
+		}
+		fmt.Printf("The first names of bookings are : %v\n", firstNames)
+
+		if remainingTickets ==0{
+		fmt.Print("tickets are sold out,Come back next time");
+		break;
+	}
+	}
+
+	
+	
 }
